5f446827d392ce0c7e2b5c55f22afe17cdad7aac
sireesha7412 <saisireesha77777@gmail.com>
2024-08-12 15:19:25 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,37 +1,41 @@
 #include "battery_check.h"
 #include "language_support.h"
+
 #define MIN_TEMPERATURE 0.0
 #define MAX_TEMPERATURE 45.0
 #define MIN_SOC 20.0
 #define MAX_SOC 80.0
 #define MAX_CHARGE_RATE 0.8
 
-int isTemperatureOk(float temperature, int lang) {
-    if (temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
-        printf("%s\n", getTemperatureOutOfRangeMessage(lang));
+typedef enum {
+    TEMPERATURE_OUT_OF_RANGE,
+    SOC_OUT_OF_RANGE,
+    CHARGE_RATE_OUT_OF_RANGE
+} MessageType;
+
+int checkParameter(float value, float min, float max, MessageType messageType, Language lang) {
+    if (value < min || value > max) {
+        printf("%s\n", getErrorMessage(lang, messageType));
         return 0; // Not okay
     }
     return 1; // Okay
 }
 
-int isSocOk(float soc, int lang) {
-    if (soc < MIN_SOC || soc > MAX_SOC) {
-        printf("%s\n", getSocOutOfRangeMessage(lang));
-        return 0; // Not okay
-    }
-    return 1; // Okay
+int isTemperatureOk(float temperature, Language lang) {
+    return checkParameter(temperature, MIN_TEMPERATURE, MAX_TEMPERATURE, TEMPERATURE_OUT_OF_RANGE, lang);
 }
 
-int isChargeRateOk(float chargeRate, int lang) {
-    if (chargeRate > MAX_CHARGE_RATE) {
-        printf("%s\n", getChargeRateOutOfRangeMessage(lang));
-        return 0; // Not okay
-    }
-    return 1; // Okay
+int isSocOk(float soc, Language lang) {
+    return checkParameter(soc, MIN_SOC, MAX_SOC, SOC_OUT_OF_RANGE, lang);
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate, int lang) {
+int isChargeRateOk(float chargeRate, Language lang) {
+    return checkParameter(chargeRate, 0.0, MAX_CHARGE_RATE, CHARGE_RATE_OUT_OF_RANGE, lang);
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate, Language lang) {
     return isTemperatureOk(temperature, lang) &&
            isSocOk(soc, lang) &&
            isChargeRateOk(chargeRate, lang);
 }
+
